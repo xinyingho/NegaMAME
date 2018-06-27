6064a4d41b9f3ae0c192d7b5870bc4275143c756
Cyril APAN <cyril.apan@babelsoft.net>
2018-06-27 09:59:30 +0200

    workaround for empty parameters being badly passed down from Java to MAME on Linux and macOS

--- src/osd/osdcore.cpp
+++ src/osd/osdcore.cpp
@@ -235,9 +235,13 @@ std::vector<std::string> osd_get_command_line(int argc, char *argv[])
 	{
 		// for non Windows platforms, we are assuming that arguments are
 		// already UTF-8; we just need to convert to std::vector<std::string>
+		std::string empty_args("\"\"");
 		results.reserve(argc);
 		for (int i = 0; i < argc; i++)
-			results.emplace_back(argv[i]);
+			if (empty_args.compare(argv[i]) != 0)
+				results.emplace_back(argv[i]);
+			else // Negatron: workaround for a Java bug where empty parameters are passed as "\"\"" instead of "" as expected
+				results.emplace_back("");
 	}
 #endif // WIN32
 	return results;

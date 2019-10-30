e6e19f95987d566fb2c089248606093332fb1fe2
Cyril APAN <cyril.apan@babelsoft.net>
2019-10-30 08:17:08 +0100

    added the command line option -listmediaxml

--- src/frontend/mame/info.h
+++ src/frontend/mame/info.h
@@ -35,6 +35,7 @@ public:
 	// output
 	void output(std::ostream &out, const std::vector<std::string> &patterns);
 	void output(std::ostream &out, const std::function<bool(const char *shortname, bool &done)> &filter = { }, bool include_devices = true);
+	void output_media(std::ostream &out, driver_enumerator &drivlist);
 
 private:
 	// internal state
